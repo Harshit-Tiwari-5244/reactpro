254b6303599f94f0cdf150533cbda897cff16928
Harshit-Tiwari-5244 <147434248+Harshit-Tiwari-5244@users.noreply.github.com>
2024-10-27 02:22:15 +0530

    react first program

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1>Welcome in react world</h1>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
