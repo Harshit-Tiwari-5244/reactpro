60ad0edfea8d79a84ec1327778962c4120b55d8e
Harshit-Tiwari-5244 <147434248+Harshit-Tiwari-5244@users.noreply.github.com>
2024-10-30 03:04:44 +0530

    second update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// var React = require('react');
+// var ReactDom = require('react-dom');
+// ReactDom.render('what to show','where to show','call back function')
+// ReactDom.render(<h1>Hello Aman!!</h1>,document.getElementById('root'))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>Welcome in react world</h1>
-);
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// ReactDom.render(<h1>Hello Aman!<br></br> How are you???</h1>, document.getElementById('root'));
+
+// how browser understand above code
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// ReactDom.render(
+//     React.createElement("h1",null,"Hello Aman!! How are you??"),document.getElementById("root")
+// );
 
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// by using pure js
+let h1 = document.getElementById("h1");
+h1.innerHTML = "Hello Aman!!How are you";
+document.getElementById("root").appendChild(h1);
